913f87ae760dc64add29aed2d044f8aa5c27f6b3
vinoduiexpect <vinod.uiexpect@gmail.com>
2018-02-07 15:27:26 +0530

    js testing

---
+++ spec/firsttest.js
@@ -0,0 +1,10 @@
+describe("Test a Javascript Variable", function(){
+	var a = 1;
+	it("Check Variable is Defined or not", function(){
+		expect(typeof a).toBe('number');
+	})
+
+	it("Check Variable a is equal to 1", function(){
+		expect(a).toEqual(2);
+	})
+})
