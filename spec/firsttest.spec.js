64d865f111b0c8b5175acfedafae6d13dad1c8f9
vinoduiexpect <vinod.uiexpect@gmail.com>
2018-02-09 14:43:31 +0530

    Added one more change for testing

--- spec/firsttest.spec.js
+++ spec/firsttest.spec.js
@@ -1,10 +1,15 @@
 describe("Test a Javascript Variable", function(){
 	var a = 1;
+	var b = 1;
 	it("Check Variable is Defined or not", function(){
 		expect(typeof a).toBe('number');
 	})
 
 	it("Check Variable a is equal to 1", function(){
 		expect(a).toEqual(1);
+	})
+
+	it("Check Variable a and b values are equal", function(){
+		expect(a).toEqual(b);
 	})
 })
